4264ffe95ba3d0901821692796dda32d6872a96a
Sieve Lau <sievelau@gmail.com>
2022-12-04 11:41:05 +0800

    init

---
+++ Node.cpp
@@ -0,0 +1,7 @@
+//
+// Created by Sieve Lau on 2022/12/4.
+//
+
+#include "Node.h"
+
+namespace simplexml {}// namespace simplexml
---
+++ Node.h
@@ -0,0 +1,19 @@
+//
+// Created by Sieve Lau on 2022/12/4.
+//
+
+#ifndef RWSIMPLIFIEDPATCHER__NODE_H
+#define RWSIMPLIFIEDPATCHER__NODE_H
+#include <string>
+#include "helper/helper.hpp"
+
+namespace simplexml {
+
+class Node {
+  protected:
+    std::wstring def_name;
+};
+
+}// namespace simplexml
+
+#endif//RWSIMPLIFIEDPATCHER__NODE_H
